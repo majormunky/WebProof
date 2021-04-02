96f6a9ff1bd28223a6ea0e0208a7e55dfc888ab3
Josh Bright <josh.bright@gmail.com>
2021-04-01 17:16:08 -0700

    Added function to generate a uuid

--- WebProof.js
+++ WebProof.js
@@ -1,8 +1,16 @@
 let app;
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 class WebProofToolbox {
     constructor() {
         this.canvas = null;
+        this.notes = {};
         this.setup_toolbox();
         this.setup_handlers();
     }
